0ba1f99943f9626f0fe87e2304a808945eeebdbc
tunguyennnnn <conghoa522@gmail.com>
2019-02-10 13:01:06 -0500

    Add webpack cofiguration

---
+++ client/webpack/config.path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+  root: path.resolve(__dirname, '../'),
+  outputPath: path.resolve(__dirname, '..', '..', 'server/public'),
+  entryPath: path.resolve(__dirname, '..', 'src/index.js'),
+  templatePath: path.resolve(__dirname, '..', 'src/index.html')
+};
