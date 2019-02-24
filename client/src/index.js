eb9e6b95589fbe57dd511f74809c039c16e2cc58
tunguyennnnn <conghoa522@gmail.com>
2019-02-24 00:36:14 -0500

    Setup graphql, wrap Writing component

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
 
+import client from './apolloClient';
 import App from './App';
 
 ReactDom.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
