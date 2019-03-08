3430b38d57464e85fb1ed3399e51654d390d1b56
tunguyennnnn <conghoa522@gmail.com>
2019-03-07 23:24:51 -0500

    Add semantic ui

--- client/src/components/chapterlist/Chapter.js
+++ client/src/components/chapterlist/Chapter.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, Icon, Image } from 'semantic-ui-react';
 
 export default function Chapter({ children, link }) {
   return (
-    <div class="chapter-content">
-      <div class="title">
-        <Link to={link}>{children}</Link>
-      </div>
-      <p class="subtitle">Some description about the chapter</p>
-    </div>
+    <Card className="chapter-content">
+      <Card.Content>
+        <Card.Header>
+          <Link class="" to={link}>
+            {children}
+          </Link>
+        </Card.Header>
+        <Card.Meta>
+          <span className="date">Written in 2019</span>
+        </Card.Meta>
+      </Card.Content>
+      <Card.Content>
+        <a>
+          <Icon name="user" />
+          150 views
+        </a>
+      </Card.Content>
+    </Card>
   );
 }
