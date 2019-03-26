2044cbe9c4827ab89e88f9384d36b09ede9ff8a5
tunguyennnnn <conghoa522@gmail.com>
2019-03-26 01:01:35 -0400

    More styling on card

--- client/src/components/commons/Card.js
+++ client/src/components/commons/Card.js
@@ -1,12 +1,21 @@
 import './card.scss';
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import * as UserId from '../../utils/userId';
 import BlogEditor from '../BlogEditor';
 
 export default function Card(props) {
   const { title, body, type, author, view, like } = props;
+  const link = '/of/' + UserId.generateId(author.id, author.fullname);
   return (
     <div class="card-container">
-      <div class="card-author">{author.fullname}</div>
+      <div class="card-author">
+        <Link to={link} className="scaler-hover">
+          <Icon name="id badge outline" />
+          {author.fullname}
+        </Link>
+      </div>
       <div class="card-content-container">
         <div class="title">
           <strong>{title}</strong>
