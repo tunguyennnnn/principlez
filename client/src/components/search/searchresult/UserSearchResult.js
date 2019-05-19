dcf19dc032fe3b73a6fe6533a25bd994669e5d45
vanessa <vcianciarelli@gmail.com>
2019-05-18 20:48:20 -0400

    added links to users from search

--- client/src/components/search/searchresult/UserSearchResult.js
+++ client/src/components/search/searchresult/UserSearchResult.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as UserId from '../../../utils/userId';
 
 function displayResults(results, limit) {
   return _.map(_.take(results, limit), (result, index) => {
-    const { fullname } = result;
-    return <li key={index}>@{fullname}</li>;
+    const { fullname, id } = result;
+    return (
+      <li key={index}>
+        <Link to={`/of/${UserId.generateId(id, fullname)}`}>@{fullname}</Link>
+      </li>
+    );
   });
 }
 
